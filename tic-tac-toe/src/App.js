796599a2fb9f991fe2dfc62714a7349a2c319252
isanasan <yfcgpsebp@gmail.com>
2024-12-29 22:25:55 +0900

    https://ja.react.dev/learn/tutorial-tic-tac-toe#showing-the-past-moves

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -10,13 +10,32 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo() {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}></button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <al>{/* TODO */}</al>
+        <al>{moves}</al>
       </div>
     </div>
   )

aeb69094d42ccbf2605a27d3baee7fdd71c5b2c3
isanasan <yfcgpsebp@gmail.com>
2024-12-29 22:17:56 +0900

    https://ja.react.dev/learn/tutorial-tic-tac-toe#adding-time-travel

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,15 +1,34 @@
 import { useState } from "react";
 
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <al>{/* TODO */}</al>
+      </div>
+    </div>
+  )
+}
+
 function Square({value, onSquareClick}) {
   return <button className="square" onClick={onSquareClick}>
     {value}
   </button>;
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setsquares] = useState(Array(9).fill(null));
-
+function Board({xIsNext, squares, onPlay}) {
   function handleClick(i) {
     if (squares[i]) {
       return;
@@ -20,8 +39,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setsquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
